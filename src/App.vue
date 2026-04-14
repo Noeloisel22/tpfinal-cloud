1b80f1bef0f37ab19e1044035d531164577f357f
Noeloisel22 <noe.loisel102@gmail.com>
2026-04-14 14:40:47 +0200

    test de modification

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,6 @@ const today = new Date().toLocaleDateString('fr-FR')
     <p>Nom : Noé Loisel</p>
     <p>Date : {{ today }}</p>
     <p>Build : {{ buildId }}</p>
+    <p>Test modif</p>
   </header>
 </template>
